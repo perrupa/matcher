298f594179bcbf63482dbb2a5bcd696b7341a6f7
Chris Marlow <chris.marlow@achievers.com>
2015-06-14 17:47:47 -0400

    Added value checking for single and multiple properties

--- Matcher.php
+++ Matcher.php
@@ -2,12 +2,39 @@
 namespace Matcher;
 
 class ArrayMatcher {
-	function __construct( $subject ) {
-		$this->subject = $subject;
-	}
+  function __construct( $subject ) {
+    $this->subject = $subject;
+  }
 
-	public function hasProperty($value='')
-	{
-		return array_key_exists( $value, $this->subject );
-	}
+  public function hasProperty($property='') {
+    return array_key_exists( $property, $this->subject );
+  }
+
+  public function hasProperties($properties='') {
+    $results = array_map(function($prop){
+        return $this->hasProperty($prop);
+      }, $properties);
+
+    return array_search( false, $results ) === false;
+  }
+
+  public function hasValue( $key, $value ) {
+    if( !$this->hasProperty($key) ) return false;
+    return $this->subject[$key] === $value;
+  }
+
+  public function hasValues($keyValuePairs=[]) {
+    $keys = array_keys($keyValuePairs);
+    $values = array_values($keyValuePairs);
+
+    $results = array_map(function($key, $value){
+        return $this->hasValue( $key, $value );
+      }, $keys, $values);
+
+    return array_search( false, $results ) === false;
+  }
+}
+
+function log($obj) {
+  echo "\n" . print_r($obj, true);
 }
--- test_Matcher.php
+++ test_Matcher.php
@@ -11,9 +11,59 @@ class test_ArrayMatcher extends PHPUnit_Framework_TestCase {
 		];
 	}
 
-	function test_hasProperty() {
-		$m = new ArrayMatcher( $this->little_girl );
-		$this->assertTrue( $m->hasProperty("name"), 'verifies a property exists.' );
-	}
+    function test_hasProperty() {
+        $m = new ArrayMatcher( $this->little_girl );
+        $this->assertTrue(
+            $m->hasProperty("name"),
+            'verifies a property exists.');
+        $this->assertFalse(
+            $m->hasProperty("SIN#"),
+            'verifies a property does not exist.');
+    }
+
+    function test_hasProperties() {
+        $m = new ArrayMatcher( $this->little_girl );
+        $this->assertTrue(
+            $m->hasProperties(["name", "age"]),
+            'verifies a property exists.');
+        $this->assertFalse(
+            $m->hasProperties(["name", "SIN#"]),
+            'verifies a property does not exist.');
+    }
+
+    function test_hasValue() {
+        $m = new ArrayMatcher( $this->little_girl );
+        $this->assertTrue(
+            $m->hasValue("name", "Suzanna"),
+            'verifies a property\'s value is the same.');
+        $this->assertFalse(
+            $m->hasValue("name", "Bobby"),
+            'verifies a property\'s value is not the same.');
+    }
+
+    function test_hasValues() {
+        $m = new ArrayMatcher( $this->little_girl );
+
+        $this->assertTrue(
+            $m->hasValues(["name" =>  "Suzanna"]),
+            'verifies a property\'s single value is the same.');
+        $this->assertFalse(
+            $m->hasValues(["name" =>  "Bobby"]),
+            'verifies a property\'s single value is not the same.');
+
+        $this->assertTrue(
+            $m->hasValues([
+                "name" =>  "Suzanna",
+                "age"  => 12
+            ]),
+            'verifies a property\'s multiple values are the same.');
+        $this->assertFalse(
+            $m->hasValues([
+                "name" =>  "Bobby",
+                "age"  => 12
+            ]),
+            'verifies a property\'s multiple values are not the same.');
+    }
 
 }
+
